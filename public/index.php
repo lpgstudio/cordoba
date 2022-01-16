08b48e1b6fe3c6759699c204eafd63f5f6a29514
Thiago de Paula <thiago_newdesign@hotmail.com>
2022-01-15 22:51:35 -0300

    institucional e categoria

--- public/index.php
+++ public/index.php
@@ -4,10 +4,11 @@ include_once '../src/views/templates/header.php';
 
 // include_once '../src/views/como-comprar.php';
 // include_once '../src/views/politica-de-entrega.php';
-// include_once '../src/views/politica-de-privacidade.php';
+// include_once '../src/views/rastreamento-de-pedido.php';
+include_once '../src/views/categoria.php';
 
-include_once '../src/views/templates/banner_principal.php';
-include_once '../src/views/templates/destaques.php';
+// include_once '../src/views/templates/banner_principal.php';
+// include_once '../src/views/templates/destaques.php';
 include_once '../src/views/templates/promocao.php';
 include_once '../src/views/templates/newsleater.php';
 include_once '../src/views/templates/footer.php';
---
+++ src/views/fale-conosco.php
@@ -0,0 +1,29 @@
+<section id="fale-conosco" class="institucional">
+    <div class="container">
+        <div class="box-menu">
+            <h3>
+                Podemos <br>Te Ajudar<br>
+                em Algo?
+            </h3>
+            <a href="#">Prazo de Entrega</a>
+            <a href="#">Formas de Pagamento</a>
+            <a href="#">Políticas de Entrega</a>
+            <a href="#">Dúvidas Frequentes</a>
+            <a href="#">Trocas e Devoluções</a>
+        </div>
+        <div class="form-fale-conosco">
+            <h3>Fale Conosco</h3>
+            <form action="#">
+                <label for="nome-fale-conosco">*Nome</label>
+                <input type="text" id="nome-fale-conosco" name="nome-fale-conosco" placeholder="Nome Completo" required>
+                <label for="email-fale-conosco">*Email</label>
+                <input type="email" id="email-fale-conosco" name="email-fale-conosco" placeholder="Digite um e-mail válido" required>
+                <label for="telefone-fale-conosco">*Telefone</label>
+                <input type="phone" id="telefone-fale-conosco"  placeholder="11 9 8888-8888" name="telefone-fale-conosco" required>
+                <label for="mensagem-fale-conosco">*Mensagem</label>
+                <textarea name="mensagem-fale-conosco" id="mensagem-fale-conosco" placeholder="Digite aqui sua mensagem"required></textarea>
+                <button>Enviar</button>
+            </form>
+        </div>
+    </div>
+</section>
